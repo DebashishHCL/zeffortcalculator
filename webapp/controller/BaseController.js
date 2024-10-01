98e1a818bc290155a1f018f6deb31dc649d55a53
Debashish Das <debashish.das@hcltech.com>
2024-10-01 07:26:29 +0000

    first commit

---
+++ webapp/controller/BaseController.js
@@ -0,0 +1,90 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"../utils/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, History, formatter, Filter, FilterOperator) {
+	"use strict";
+	return Controller.extend("com.zeffortcalculator.controller.BaseController", {
+		formatter: formatter,
+		getRouter: function () {
+			return this.getOwnerComponent().getRouter();
+		},
+
+		getOwnerModel: function (sName) {
+			return this.getOwnerComponent().getModel(sName);
+		},
+
+		setOwnerModel: function (oModel, sName) {
+			return this.getOwnerComponent().setModel(oModel, sName);
+		},
+
+		getResourceBundle: function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+
+		onNavBack: function () {
+			var sPreviousHash = History.getInstance().getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("Home", {}, false);
+			}
+		},
+
+		createFilter: function (path, value) {
+			return new Filter({
+				path: path,
+				operator: FilterOperator.EQ,
+				value1: value
+			});
+		},
+
+		specialCharacterCheck: function (sValue) {
+			const specialCharRegex = /^[a-zA-Z0-9 .]+$/;
+			if (!specialCharRegex.test(sValue)) return false;
+			else return true;
+		},
+
+		callBackEnd: function (sEntitySet, sAction, aFilter, oPayLoad, oUrlParameters) {
+			switch (sAction) {
+				case "GET":
+					return new Promise((resolve, reject) => {
+						this.getOwnerModel().read(sEntitySet, {
+							filters: aFilter,
+							urlParameters: oUrlParameters,
+							success: function (oData, oResponse) {
+								resolve(oResponse);
+							},
+							error: function (error) {
+								reject(error);
+							}
+						});
+
+					});
+					break;
+				case "POST":
+					return new Promise((resolve, reject) => {
+						this.getOwnerModel().create(sEntitySet, oPayLoad, {
+							success: function (oData, oResponse) {
+								resolve(oResponse);
+							},
+							error: function (e, oResponse) {
+								reject(e);
+							}
+						});
+
+					});
+					break;
+				default:
+					break;
+			}
+		},
+
+		onHome: function () {
+			this.getRouter().navTo("Home", {}, false);
+		}
+	});
+});
---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+	"./BaseController",
+	"sap/ui/core/library",
+	"../utils/formatter"
+],
+	function (BaseController, CoreLib, formatter ) {
+		"use strict";
+		const { BusyIndicator } = CoreLib;
+		return BaseController.extend("com.zeffortcalculator.controller.Detail", {
+			formatter: formatter,
+			onInit: function () {
+				this.getView().addEventDelegate({
+                    onAfterShow: ()=>{
+                       this.getView().byId("hclImg").attachPress(()=>{
+                            this.onHome();
+                       });
+                    }
+                });
+				this.getRouter().getRoute("Detail").attachPatternMatched(this.onDetailMatched, this);
+				this.getView().byId("detailEdit").setVisible(true);
+			},
+			onEdit: function (sSaveorEdit) {
+				this.getOwnerModel("oModelEstCal").setProperty("/SaveOrEdit", sSaveorEdit);
+				this.getRouter().navTo("Calculate", {}, false);
+			},
+			onSearchCust: function () {
+				this.getRouter().navTo("Search", {
+					OpportunityId: this.getOwnerModel("oModelEstCal").getProperty("/OpportunityId")
+				},false);
+			},
+
+			onDetailMatched: function (oEvent) {
+				if (this.getView().byId("detailSave"))
+					this.getView().byId("detailSave").setVisible(true);
+	
+				if (oEvent.getParameter("arguments").CustId && oEvent.getParameter("arguments").OpportunityId && oEvent.getParameter("arguments").Version) {
+					if (this.getView().byId("detailSave"))
+						this.getView().byId("detailSave").setVisible(false);
+	
+					let filters = [this.createFilter("CustId", oEvent.getParameter("arguments").CustId),
+					this.createFilter("OpportunityId", oEvent.getParameter("arguments").OpportunityId),
+					this.createFilter("Version", oEvent.getParameter("arguments").Version)
+					];
+					BusyIndicator.show();
+					let oDetailData = this.callBackEnd("/zi_hcl_header", "GET", filters, {}, {});
+	
+					oDetailData.then((oResponse) => {
+						let response = oResponse.data;
+						this.getOwnerModel("oModelEstCal").setData({});
+						this.getOwnerModel("oModelEstCal").setData(response.results[0]);
+						BusyIndicator.hide();
+					}).catch((error) => {
+						BusyIndicator.hide();
+						console.log(error);
+					});
+				}
+			}
+			
+		});
+	});
